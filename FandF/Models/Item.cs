66330b4a5d69f3b43fc3300a4c3a7d99de7ed648
keegdragon <keegdragon@users.noreply.github.com>
2017-04-11 16:27:54 -0700

    Adding updated Item.cs

--- FandF/Models/Item.cs
+++ FandF/Models/Item.cs
@@ -6,7 +6,32 @@ namespace FandF
 {
 	public class Item : ObservableObject
 	{
-		string id = string.Empty;
+        private int Str;
+        private int Def;
+        private int Dex;
+        private int Health;
+
+        public int getStr()
+        {
+            return Str;
+        }
+
+        public int getDef()
+        {
+            return Def;
+        }
+
+        public int getDex()
+        {
+            return Dex;
+        }
+
+        public int getHealth()
+        {
+            return Health;
+        }
+
+        string id = string.Empty;
 
 		[JsonIgnore]
 		public string Id
